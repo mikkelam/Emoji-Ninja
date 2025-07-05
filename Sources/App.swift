a8dc14d269d752d07826972ca924897a24cfef02
mikkelam <mikkelarm@gmail.com>
2025-07-05 11:17:20 +0200

    appicon

--- Sources/App.swift
+++ Sources/App.swift
@@ -1,3 +1,4 @@
+import AppKit
 import EmojiKit
 import ServiceManagement
 import SwiftUI
@@ -7,9 +8,37 @@ struct EQuipApp: App {
     @StateObject private var appState = AppState()
     @StateObject private var emojiManager = EmojiManager()
 
+    private var menuBarImage: NSImage {
+        let image = NSImage(size: NSSize(width: 18, height: 18))
+        image.lockFocus()
+
+        let emoji = "⚔️"
+        let font = NSFont.systemFont(ofSize: 16)
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: font,
+            .foregroundColor: NSColor.controlTextColor,
+        ]
+
+        let size = emoji.size(withAttributes: attributes)
+        let rect = NSRect(
+            x: (18 - size.width) / 2,
+            y: (18 - size.height) / 2,
+            width: size.width,
+            height: size.height
+        )
+
+        emoji.draw(in: rect, withAttributes: attributes)
+        image.unlockFocus()
+
+        image.isTemplate = true
+        return image
+    }
+
     var body: some Scene {
-        MenuBarExtra("E-quip", systemImage: "face.smiling") {
+        MenuBarExtra {
             MenuBarView(appState: appState, emojiManager: emojiManager)
+        } label: {
+            Image(nsImage: menuBarImage)
         }
         .menuBarExtraStyle(.menu)
     }
