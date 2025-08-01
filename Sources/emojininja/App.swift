744e784ec29ff09ba3bb6e775c58c744613b461e
mikkelam <mikkelarm@gmail.com>
2025-08-01 11:47:24 +0200

    bundle fixes

--- Sources/emojininja/App.swift
+++ Sources/emojininja/App.swift
@@ -5,48 +5,45 @@ import ninjalib
 
 @main
 struct EmojiNinjaApp: App {
-  @StateObject private var appState = AppState()
-  @StateObject private var emojiManager = EmojiManager()
-  @StateObject private var themeManager = ThemeManager.shared
+    @StateObject private var appState = AppState()
+    @StateObject private var emojiManager = EmojiManager()
+    @StateObject private var themeManager = ThemeManager.shared
 
-  init() {
-    // Log emoji data stats on startup
-    logEmojiDataStats()
-  }
-
-  private var menuBarImage: NSImage {
-    guard
-      let bundleURL = Bundle.main.url(
-        forResource: "Emoji Ninja_emoji", withExtension: "bundle"),
-      let bundle = Bundle(url: bundleURL),
-      let imageURL = bundle.url(forResource: "ninja_menu", withExtension: "png"),
-      let image = NSImage(contentsOf: imageURL)
-    else {
-      fatalError("ninja_menu.png not found")
+    init() {
+        // Log emoji data stats on startup
+        logEmojiDataStats()
     }
-    image.size = NSSize(width: 16, height: 16)
 
-    return image
-  }
-
-  var body: some Scene {
-    MenuBarExtra {
-      MenuBarView(appState: appState, emojiManager: emojiManager, themeManager: themeManager)
-        .themedEnvironment(themeManager)
-        .onAppear {
-          // Check accessibility permissions status on startup
-          _ = appState.checkAccessibilityPermissions()
+    private var menuBarImage: NSImage {
+        guard
+            let imageURL = Bundle.module.url(forResource: "ninja_menu", withExtension: "png"),
+            let image = NSImage(contentsOf: imageURL)
+        else {
+            fatalError("ninja_menu.png not found")
         }
-    } label: {
-      Image(nsImage: menuBarImage)
-    }
-    .menuBarExtraStyle(.menu)
-  }
+        image.size = NSSize(width: 16, height: 16)
 
-  @MainActor
-  private func logEmojiDataStats() {
-    let dataManager = AppEmojiManager.shared
-    _ = dataManager.getAllEmojis()
-    _ = dataManager.getAvailableGroups()
-  }
+        return image
+    }
+
+    var body: some Scene {
+        MenuBarExtra {
+            MenuBarView(appState: appState, emojiManager: emojiManager, themeManager: themeManager)
+                .themedEnvironment(themeManager)
+                .onAppear {
+                    // Check accessibility permissions status on startup
+                    _ = appState.checkAccessibilityPermissions()
+                }
+        } label: {
+            Image(nsImage: menuBarImage)
+        }
+        .menuBarExtraStyle(.menu)
+    }
+
+    @MainActor
+    private func logEmojiDataStats() {
+        let dataManager = AppEmojiManager.shared
+        _ = dataManager.getAllEmojis()
+        _ = dataManager.getAvailableGroups()
+    }
 }
